9c74e35ac5ce59e9a9b4f5c2b098f20e6a942613
Moris Shalon <18584322+ZChuckMoris@users.noreply.github.com>
2020-06-12 04:02:23 +0300

    first commit

---
+++ Kids/chapter2/SquareSpiral1.py
@@ -0,0 +1,7 @@
+# SquareSpiral1.py
+import turtle
+t = turtle.Pen()
+for x in range(1000):
+	t.forward(x)
+	t.left(90)
+inputfield = input("input something: ")
---
+++ Kids/chapter2/SquareSpiral2.py
@@ -0,0 +1,7 @@
+# SquareSpiral1.py
+import turtle
+t = turtle.Pen()
+for x in range(300):
+	t.forward(x)
+	t.left(91)
+inputfield = input("input something: ")
